8e231bbd58f9e510e4d2968e2992bfde0712f197
DorShabat <dorshabat55@gmail.com>
2024-02-25 12:35:54 +0200

    added images

--- recipes.js
+++ recipes.js
@@ -21,6 +21,8 @@ var recipes = {
             "6. Remove from heat and quickly stir in the egg and cheese mixture. Toss until creamy and well combined.",
             "7. Crumble crispy pancetta over the top and serve hot."
         ],
+        "duration": [30, 10, 10, 5, 5, 5, 5],
+
         "pics": [
             "images/spaghetti/instruction1.jpg",
             "images/spaghetti/instruction2.jpg",
@@ -31,6 +33,7 @@ var recipes = {
             "images/spaghetti/instruction7.jpg",
             "images/finished_dish.jpg"
         ]
+
     },
     "2": {
         "title": "Chicken",
@@ -53,6 +56,8 @@ var recipes = {
             "4. In the same skillet, add heavy cream and grated Parmesan cheese. Cook until sauce thickens.",
             "5. Slice cooked chicken into strips and add it back to the skillet.",
         ],
+        "duration": [30, 5, 10, 10, 5],
+
         "pics": [
             "images/chicken/instruction1.jpg",
             "images/chicken/instruction2.jpg",
@@ -87,6 +92,7 @@ var recipes = {
             "5. Pour the sauce over the vegetables and toss to coat. Cook until the sauce thickens.",
             "6. Serve hot over cooked rice."
         ],
+        "duration": [10, 5, 5, 10, 5, 5],
         "pics": [
             "images/Vegetable Stir-Fry/instruction1.jpg",
             "images/Vegetable Stir-Fry/instruction2.jpg",
@@ -94,6 +100,204 @@ var recipes = {
             "images/Vegetable Stir-Fry/instruction4.jpg",
             "images/Vegetable Stir-Fry/instruction5.JPG",
             "images/Vegetable Stir-Fry/Vegetable Stir-Fry.jpg"
+        ]
+    },
+    "4": {
+        "title": "Pancakes",
+        "image": "images/pancakes/pancakes.jpg",
+        "allergies": "Dairy, Gluten",
+        "difficulty": "Easy",
+        "ingredients": [
+            "1 1/2 cups all-purpose flour",
+            "3 1/2 teaspoons baking powder",
+            "1 teaspoon salt",
+            "1 tablespoon white sugar",
+            "1 1/4 cups milk",
+            "1 egg",
+            "3 tablespoons butter, melted"
+        ],
+        "instructions": [
+            "1. In a large bowl, sift together the flour, baking powder, salt, and sugar.",
+            "2. Make a well in the center and pour in the milk, egg, and melted butter.",
+            "3. Mix until smooth.",
+            "4. Heat a lightly oiled griddle or frying pan over medium-high heat.",
+            "5. Pour or scoop the batter onto the griddle, using approximately 1/4 cup for each pancake.",
+            "6. Brown on both sides and serve hot."
+        ],
+        "duration": [5, 5, 5, 5, 5, 5],
+        "pics": [
+            "images/pancakes/instruction1.jpg",
+            "images/pancakes/instruction2.jpg",
+            "images/pancakes/instruction3.jpg",
+            "images/pancakes/instruction4.jpg",
+            "images/pancakes/instruction5.jpg",
+            "images/pancakes/instruction6.jpg"
+        ]
+    },
+    "5": {
+        "title": "Salad",
+        "image": "images/salad/salad.jpg",
+        "allergies": "None",
+        "difficulty": "Easy",
+        "ingredients": [
+            "4 cups mixed greens",
+            "1 cup cherry tomatoes, halved",
+            "1/2 cup cucumber, sliced",
+            "1/4 cup red onion, sliced",
+            "1/4 cup feta cheese",
+            "1/4 cup balsamic vinaigrette"
+        ],
+        "instructions": [
+            "1. In a large bowl, toss mixed greens, cherry tomatoes, cucumber, and red onion together.",
+            "2. Drizzle with balsamic vinaigrette and toss to coat.",
+            "3. Top with crumbled feta cheese and serve immediately."
+        ],
+        "duration": [5, 5, 5],
+        "pics": [
+            "images/salad/instruction1.jpg",
+            "images/salad/instruction2.jpg",
+            "images/salad/instruction3.jpg"
+        ]
+    },
+    "6": {
+        "title": "Kosher Beef Burger",
+        "image": "images/kosher_burger/kosher_burger.jpg",
+        "allergies": "None",
+        "difficulty": "Intermediate",
+        "ingredients": [
+          "1 lb kosher ground beef",
+          "4 kosher hamburger buns",
+          "4 slices of kosher cheese (optional, ensure dairy-free if serving with meat)",
+          "1/4 cup lettuce, shredded",
+          "1/4 cup tomato, sliced",
+          "1/4 cup onion, sliced",
+          "1/4 cup pickles, sliced",
+          "2 tablespoons olive oil (for grilling)",
+          "Kosher salt and black pepper to taste",
+          "Mustard and ketchup (check for kosher certification)"
+        ],
+        "instructions": [
+          "1. Divide kosher ground beef into 4 equal portions and shape into patties.",
+          "2. Season patties with kosher salt and black pepper.",
+          "3. Heat a grill pan over medium-high heat and brush with olive oil. Grill patties until cooked to your desired doneness.",
+          "4. Toast hamburger buns on the grill until lightly golden.",
+          "5. Spread mustard and ketchup on the bottom half of each bun.",
+          "6. Layer each bun with lettuce, tomato, onion, pickles, and a grilled patty.",
+          "7. If using cheese and keeping kosher, ensure it's a non-dairy cheese alternative. Place it on the patty right before it finishes cooking to melt slightly.",
+          "8. Top with the other half of the bun and serve immediately."
+        ],
+        "duration": [10, 5, 10, 5, 5, 5, 5, 5],
+        "pics": [
+          "images/kosher_burger/instruction1.jpg",
+          "images/kosher_burger/instruction2.jpg",
+          "images/kosher_burger/instruction3.jpg",
+          "images/kosher_burger/instruction4.jpg",
+          "images/kosher_burger/instruction5.jpg",
+          "images/kosher_burger/instruction6.jpg",
+          "images/kosher_burger/instruction7.jpg",
+          "images/kosher_burger/finished_dish.jpg"
+        ]
+    },
+      
+    "7": {
+        "title": "Tacos",
+        "image": "images/tacos/tacos.jpg",
+        "allergies": "Dairy, Gluten",
+        "difficulty": "Intermediate",
+        "ingredients": [
+            "1 lb ground beef",
+            "8 taco shells",
+            "1 cup shredded lettuce",
+            "1/2 cup diced tomato",
+            "1/4 cup diced onion",
+            "1/4 cup sour cream",
+            "1/4 cup salsa",
+            "1/4 cup guacamole",
+            "1/4 cup cilantro, chopped"
+        ],
+        "instructions": [
+            "1. In a skillet, cook ground beef until browned. Season with taco seasoning.",
+            "2. Heat taco shells according to package instructions.",
+            "3. Fill each taco shell with cooked ground beef.",
+            "4. Top with lettuce, tomato, onion, sour cream, salsa, guacamole, and chopped cilantro."
+        ],
+        "duration": [10, 5, 5, 5],
+        "pics": [
+            "images/tacos/instruction1.jpg",
+            "images/tacos/instruction2.jpg",
+            "images/tacos/instruction3.jpg",
+            "images/tacos/instruction4.jpg"
+        ]
+    },
+    "8": {
+        "title": "salmon",
+        "image": "images/salmon/salmon.jpg",
+        "allergies": "None",
+        "difficulty": "Intermediate",
+        "ingredients": [
+            "4 salmon fillets",
+            "1/4 cup honey",
+            "2 tablespoons soy sauce",
+            "1 tablespoon olive oil",
+            "2 cloves garlic, minced",
+            "1 tablespoon lemon juice",
+            "Salt and black pepper to taste"
+        ],
+        "instructions": [
+            "1. In a bowl, mix honey, soy sauce, olive oil, minced garlic, lemon juice, salt, and black pepper.",
+            "2. Marinate salmon fillets in the mixture for 30 minutes.",
+            "3. Preheat the oven to 400°F (200°C).",
+            "4. Place salmon fillets on a baking sheet lined with parchment paper.",
+            "5. Bake for 12-15 minutes, or until salmon is cooked through."
+        ],
+        "duration": [5, 30, 5, 5, 15],
+        "pics": [
+            "images/salmon/instruction1.jpg",
+            "images/salmon/instruction2.jpg",
+            "images/salmon/instruction3.jpg",
+            "images/salmon/instruction4.jpg",
+            "images/salmon/instruction5.jpg"
+        ]
+    },
+    "9": {
+        "title": "chocolate cake",
+        "image": "images/chocolate_cake/chocolate_cake.jpg",
+        "allergies": "Dairy, Gluten",
+        "difficulty": "Intermediate",
+        "ingredients": [
+            "1 3/4 cups all-purpose flour",
+            "1 1/2 cups granulated sugar",
+            "3/4 cup unsweetened cocoa powder",
+            "1 1/2 teaspoons baking powder",
+            "1 1/2 teaspoons baking soda",
+            "1 teaspoon salt",
+            "2 eggs",
+            "1 cup milk",
+            "1/2 cup vegetable oil",
+            "2 teaspoons vanilla extract",
+            "1 cup boiling water",
+            "1 cup chocolate frosting"
+        ],
+        "instructions": [
+            "1. Preheat the oven to 350°F (175°C). Grease and flour two 9-inch round cake pans.",
+            "2. In a large bowl, sift together flour, sugar, cocoa powder, baking powder, baking soda, and salt.",
+            "3. Add eggs, milk, oil, and vanilla extract. Beat for 2 minutes on medium speed.",
+            "4. Stir in boiling water. The batter will be thin.",
+            "5. Pour the batter evenly into the prepared pans.",
+            "6. Bake for 30-35 minutes, or until a toothpick inserted into the center comes out clean.",
+            "7. Cool in the pans for 10 minutes, then remove to a wire rack to cool completely.",
+            "8. Frost with chocolate frosting and serve."
+        ],
+        "duration": [5, 5, 5, 5, 5, 35, 10, 5],
+        "pics": [
+            "images/chocolate_cake/instruction1.jpg",
+            "images/chocolate_cake/instruction2.jpg",
+            "images/chocolate_cake/instruction3.jpg",
+            "images/chocolate_cake/instruction4.jpg",
+            "images/chocolate_cake/instruction5.jpg",
+            "images/chocolate_cake/instruction6.jpg",
+            "images/chocolate_cake/instruction7.jpg",
+            "images/chocolate_cake/finished_dish.jpg"
         ]
     }
 }
